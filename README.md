e9af7234a633a34767a376070f2797802acab10a
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2021-01-17 11:44:28 +0100

    feature complete

--- README.md
+++ README.md
@@ -3,8 +3,83 @@ Window Watcher
 
 Small script that can detect when the active window changes
 
-Planned:
+## Configuration
 
-	* tidy the code
-	* Read config file
-	* Execute command based on window attributes
+By default the script will use "\~/.config/windowwatcher/config.yml" as it's config file, this can be overridden with the -c argument.
+
+Configuration is in yaml format and consists of a number of window rules. A sample rule could look like this
+
+```yaml
+  - name: Sublime Text
+    match:
+      application: "Sublime Text"
+      name: "untitled"
+      class_group: "Sublime_text"
+      class_instance: "sublime_text"
+      icon: "untitles"
+    command: "g815-led -a a4a4a4"
+    continue: false
+    skippable: true
+```
+
+Each rule has up to 5 elements "name", "match", "command", "continue" and "skippable"
+
+	* name - optional name, if not given the rule will be given a name based on it's position in the config file.
+	* match - optional match specification, an empty or missing "match" element means the rule matches everything.
+	* command - optional command to execute
+	* continue - true or false. indicates if rules after this rule should be processed if this rule is a match, defaults to false
+	* skippable - true or false, indicates if the command should be skipped if it is the same as the previously executed command, defaults to true.
+
+
+#### Matching windows
+
+It is possible to match 5 window attributes
+
+	* application - application name
+	* name - window title
+	* class_group - window class group name
+	* class_instance - window class instance name
+	* icon - window icon
+
+Use the -v flag to have the script print out these attributes when changing window.
+Every match element is optional and only elements present will be required to match, but every element present in a rule must match for the rule to match.
+
+It is possible to use glob matching so you can use "\*" in the elements to match. For example
+
+```yaml
+      application: "*HTTPS*Chromium"
+```
+
+
+#### Default match
+
+To create a default match just create a rule with no "match" element. This will match every window.
+
+```yaml
+  - name: Default
+    command: "g815-led -a a4a4a4"
+```
+
+
+### Example configuration
+
+```yaml
+---
+  - name: Sublime Text
+    match:
+      application: "Sublime Text"
+      class_group: "Sublime_text"
+      class_instance: "sublime_text"
+    command: "g815-led -a a4a4a4"
+
+  - name: Chromium
+    match:
+      class_group: "Chromium"
+      class_instance: "chromium"
+    command: "g815-led -a a4a4a4"
+
+  - name: Default
+    command: "g815-led -a a4a4a4"
+```
+
+For more samples see config.sample.yml
--- windowwatcher.py
+++ windowwatcher.py
@@ -2,6 +2,11 @@
 
 import gi
 import sys
+import yaml
+import argparse
+import os
+import fnmatch
+
 
 gi.require_version("Wnck", "3.0")
 gi.require_version("Gtk", "3.0")
@@ -11,75 +16,107 @@ from gi.repository import Gtk
 
 
 prev_active_window: Wnck.Window = None
+prev_command: str = None
 
-def get_app_name(window: Wnck.Window):
-	if (window is None):
-		return "-None-"
-	elif (not "get_application" in dir(window)):
-		return "-NoApp"
-	elif (not "get_name" in dir(window.get_application())):
-		return "-NoName"
-	else:
-		return window.get_application().get_name()
 
-def get_window_name(window: Wnck.Window):
-	if (window is None):
-		return "-None-"
-	elif (not "get_name" in dir(window)):
-		return "-NoName"
-	else:
-		return window.get_name()
+def printverbose(s: str):
+	if (args.verbose):
+		print(s)
 
-def get_window_class_group_name(window: Wnck.Window):
-	if (window is None):
-		return "-None-"
-	elif (not "get_class_group_name" in dir(window)):
-		return "-NoClassGroupName"
-	else:
-		return window.get_class_group_name()
 
-def get_window_class_instance_name(window: Wnck.Window):
-	if (window is None):
-		return "-None-"
-	elif (not "get_class_instance_name" in dir(window)):
-		return "-NoClassInstanceName"
-	else:
-		return window.get_class_instance_name()
+def get_window_attributes(window: Wnck.Window):
+	window_dir = dir(window)
+	attributes = []
+	if "get_application" in window_dir and "get_name" in dir(window.get_application()):
+		attributes.append(("application", window.get_application().get_name()))
+	if "get_name" in window_dir:
+		attributes.append(("name", window.get_name()))
+	if "get_class_group_name" in window_dir:
+		attributes.append(("class_group", window.get_class_group_name()))
+	if "get_class_instance_name" in window_dir:
+		attributes.append(("class_instance", window.get_class_instance_name()))
+	if "get_icon_name" in window_dir:
+		attributes.append(("icon", window.get_icon_name()))
+	return dict(attributes)
 
-def get_window_icon_name(window: Wnck.Window):
-	if (window is None):
-		return "-None-"
-	elif (not "get_icon_name" in dir(window)):
-		return "-NoIconName"
-	else:
-		return window.get_icon_name()
 
-def print_window(name: str, window: Wnck.Window):
-	if (window is not None):
-		print("  " + name)
-		print("    app: " + get_app_name(window))
-		print("    name: " + get_window_name(window))
-		print("    class group: " + get_window_class_group_name(window))
-		print("    class instance: " + get_window_class_instance_name(window))
-		print("    icon: " + get_window_icon_name(window))
-	else:
-		print("  " + name + " is None")
+def print_window_attributes(attributes):
+	if args.verbose:
+		print("    match:")
+		for k, v in attributes.items():
+			print("      " + k + ": \"" + v + "\"")
+
+
+def execute_rules(attributes):
+	global prev_command
+
+	rules_to_execute = find_matching_rules(attributes)
+	for rule in rules_to_execute:
+		command = rule["command"] if "command" in rule else None
+		if not command is None:
+			if command == prev_command and rule["skippable"]:
+				print("  - skip: " + rule["name"])
+				printverbose("    command: " + str(command))
+			else:
+				print("  - executing: " + rule["name"])
+				print("    command: " + str(command))
+				if rule["continue"]:
+					print("    continue: true")
+				os.system(command)
+				prev_command = command
+		else:
+			print("  - no-cmd: " + rule["name"])
+
+
+def find_matching_rules(attributes):
+	printverbose("")
+	rules_to_execute = []
+	for rule in rules:
+		if rule["match"] is None or all_match(rule, attributes):
+			rules_to_execute.append(rule)
+			printverbose("  - matched rule: " + str(rule))
+			if not rule["continue"]:
+				break
+	printverbose("")
+	return rules_to_execute
+
+
+def all_match(rule, attributes):
+	for k, v in rule["match"]:
+		if not v is None and not fnmatch.fnmatch(attributes[k], v):
+			return False
+	return True
+
 
 def do_active_window_changed(this_screen: Wnck.Screen, previously_active: Wnck.Window):
-	global name_change_hook, prev_active_window
+	global name_change_hook, prev_active_window, prev_attributes
 
 	print ("\nactive-window-changed")
 	active_window: Wnck.Window = this_screen.get_active_window()
-	print_window("active", active_window)
+	attributes = get_window_attributes(active_window)
 	if (prev_active_window is not None):
 		prev_active_window.disconnect(name_change_hook)
 	prev_active_window = active_window
+	prev_attributes = attributes
 	if (active_window is not None):
 		name_change_hook = active_window.connect("name-changed", do_window_name_changed)
+		print_window_attributes(attributes)
+		execute_rules(attributes)
+	else:
+		print("    window is None")
+
 
 def do_window_name_changed(active_window: Wnck.Window):
-	print ("\nwindow: name-changed")
-	print_window("active", active_window)
+	global prev_attributes
+
+	print("\nwindow: name-changed")
+	attributes = get_window_attributes(active_window)
+	print_window_attributes(attributes)
+	if (attributes != prev_attributes):
+		print("  window changed")
+		execute_rules(attributes)
+	prev_attributes = attributes
+
 
 def window_active_init():
 	Gtk.init([])
@@ -87,8 +124,55 @@ def window_active_init():
 	screen.connect("active-window-changed", do_active_window_changed)
 	Gtk.main()
 
+
+def load_config():
+	global rules
+	rules = []
+	try:
+		ruleNum = 0
+		full_path = os.path.expanduser(args.config)
+		print("Loading config file: " + full_path)
+		with open(full_path, "r") as file:
+			configfile = yaml.load(file, Loader=yaml.FullLoader)
+			for rule in configfile:
+				ruleNum = ruleNum + 1
+				name = rule["name"] if "name" in rule else "Rule " + str(ruleNum)
+				printverbose("\n  - name: " + name)
+
+				if ("match" in rule):
+					match = []
+					printverbose("    match:")
+					for k in ["application", "name", "class_group", "class_instance", "icon"]:
+						if k in rule["match"]:
+							match.append((k, rule["match"][k]))
+							printverbose("      " + k + ": " + rule["match"][k])
+						else:
+							match.append((k, None))
+				else:
+					match = None
+				command = rule["command"] if "command" in rule else None
+				printverbose("    command: " + str(command))
+				cont = rule["continue"] if "continue" in rule else False
+				printverbose("    continue: " + str(cont))
+				skippable = rule["skippable"] if "skippable" in rule else True
+
+				rules.append({"name":name, "match":match, "command":command, "continue":cont, "skippable":skippable})
+		#print(str(rules))
+		printverbose("\nConfig file parsed")
+	except IOError:
+		print("IOError: Unable to read " + args.config)
+		sys.exit(1)
+
+
 if __name__ == "__main__":
 	try:
+		parser = argparse.ArgumentParser()
+		parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+		parser.add_argument("-c", "--config", help="location of a config file", default="~/.config/windowwatcher/config.yml")
+		args = parser.parse_args()
+		if args.verbose:
+			print("Verbose")
+		load_config()
 		window_active_init()
 	except KeyboardInterrupt:
 		print("\nKeyboardInterrupt: Exit program")
